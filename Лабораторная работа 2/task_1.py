d11a884dc360aa1d780a64b87f3f68aaad23a63b
OGKABORDA <86598428+OGKABORDA@users.noreply.github.com>
2024-11-24 14:29:41 +0300

    Update task_1.py
    Убрал условие " if money_capital > 0 : break", "убрал new_spend = spend"

--- Лабораторная работа 2/task_1.py
+++ Лабораторная работа 2/task_1.py
@@ -1,15 +1,13 @@
+money_capital = 20000  # Подушка безопасности
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
-months = 10  # Количество месяцев, которое планируется протянуть без долгов
-increase = 0.03  # Ежемесячный рост цен #В условии сказано, что increase 5%
+increase = 0.05  # Ежемесячный рост цен
 
-# TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
-
-new_spend = spend
-all_salary = salary * months
-for _ in range(months - 1):
+# TODO Посчитайте количество месяцев, которое можно протянуть без долгов
+months = 0
+while money_capital >= 0:
+    money_capital -= spend - salary
+    months += 1
     spend += spend * increase
-    new_spend += spend
-money_capital = round(new_spend - all_salary)
-print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", money_capital)
 
+print("Количество месяцев, которое можно протянуть без долгов:", months - 1)
